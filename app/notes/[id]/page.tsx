3f707b453663633f3e447f52d5e6203827c68b61
Tania Lapa <taniamiroshnuchenko@gmail.com>
2026-07-26 15:15:49 +0200

    update metadata and layout

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -5,10 +5,30 @@ import {
 } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "./NoteDetails.client";
+import { Metadata } from "next";
 
 type Props = {
   params: Promise<{ id: string }>;
 };
+
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
+
+  const note = await fetchNoteById(id);
+
+  return {
+    title: note.title,
+    description: note.content,
+    openGraph: {
+      title: note.title,
+      description: note.content,
+      url: `http://localhost:3000/notes/${id}`,
+      images: ["/og-image.png"],
+    },
+  };
+}
+
 
 const NoteDetails = async ({ params }: Props) => {
   const { id } = await params;
--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from "react";
+
+
 import css from "./LayoutNotes.module.css";
+
 
 interface FilterLayoutProps {
   children: ReactNode;
   sidebar: ReactNode;
 }
-
 
 export default function RootLayout({
   children,
